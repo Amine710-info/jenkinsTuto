7fbd49dd62087e7acd93e56c1058ac17f8ca3758
Mohammed Amine Bouzid <mabouzid@smb.sqli.com>
2023-03-19 13:05:17 +0100

    email setup done

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,28 +1,34 @@
 pipeline {
     agent any
 
-  
+    triggers { pollSCM('* * * * *')
+    }
     stages {
-        stage('checkout'){
-            steps{
-             git branch: 'main', url: 'https://github.com/Amine710-info/jenkinsTuto.git'
+        stage('checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Amine710-info/jenkinsTuto.git'
             }
         }
         stage('Build') {
             steps {
-        
                 // Run Maven on a Unix agent.
-            sh "./mvnw clean package"
-
-                // To run Maven on a Windows agent, use
-                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+          //      sh './mvnw clean package'
+        sh 'true'
+            // To run Maven on a Windows agent, use
+            // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
 
             post {
-               
                 always  {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                  archiveArtifacts 'target/*.jar'
+                //    junit '**/target/surefire-reports/TEST-*.xml'
+                  //  archiveArtifacts 'target/*.jar'
+                /* }
+                 changed { */
+                   emailext attachLog: true,  
+                    body: "Please go to (build ${BUILD_URL}) ", 
+                    recipientProviders: [requestor(), upstreamDevelopers()],
+                    to: 'test@jenkins',
+                    subject:  "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ,with status ${currentBuild.result}"
                 }
             }
         }
